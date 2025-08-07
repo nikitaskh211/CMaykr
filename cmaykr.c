305e8f40e46c333adfba0b6f31a7063d5174738f
nikitaskh211 <nikitaskh211@gmail.com>
2025-08-07 16:41:23 +0300

    Rebuilt CLI for robustness.

--- cmaykr.c
+++ cmaykr.c
@@ -2,108 +2,84 @@
 #include <stdlib.h>
 #include <string.h>
 
-/// Metadata
-#define USAGE_STRING                                                           \
-  "[INF] Usage: `cmaykr` [flag] [directories]\n"                               \
+/// String constant for usage
+#define STRING_USAGE                                                           \
+  "[INF] Usage: cmaykr [flags] [directories]\n"                                \
   "[INF] Flags:\n"                                                             \
-  "        -h == Show help\n"                                                  \
-  "        -v == Show version\n"                                               \
-  "        -d == Debug build\n"                                                \
-  "        -r == Release build\n"
-#define VERSION_STRING "0.1.0\n" // major.minor.patch
+  "      -h or --help = This flag displays usage of the program.\n"            \
+  "      -v or --version = This flag displays version of the program.\n"       \
+  "      -m or --make = This flag makes each project.\n"                       \
+  "      -d or --debug = This flag builds debug version of the projects.\n"    \
+  "      -r or --release = This flag builds release version of the "           \
+  "projects.\n"
 
-/// Maximum amount of projects that can be made and build.
-#define MAX_PROJECTS 128
+/// Integer constants for version
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 1
+#define VERSION_PATCH 1
 
-/// Srting length for a path to config.maykr
-#define CONFIG_PATH_LENGTH 512
+int make_project();
+int build_debug();
+int build_release();
 
 int main(int argc, char *argv[]) {
-  /// Main variables
-  int flag_help = 0, flag_version = 0, flag_debug = 0, flag_release = 0;
-  char *projects[MAX_PROJECTS];
-  char **project = projects;
-  /// Skipping the first argument as it is the program name
+  // Skipping the first argument, which is the name of the program
   argv++;
   argc--;
-  /// Main CLI argument processor
+  // Variables for flags
+  int f_err = 0;
+  int f_help = 0, f_version = 0, f_make = 0, f_debug = 0, f_release = 0;
+  // Failsafe in case only the name of the program is used
   if (argc == 0) {
-    flag_help = 1;
-    fprintf(stderr, "[ERR] The program has no arguments.\n");
+    fprintf(stderr, "[ERR] No arguments provided to the program.\n");
+    f_help = 1;
+    f_err = 1;
   }
-  while (*argv != NULL) {
+  // Variables for directories
+  int dirc = 64;
+  char *dirv[64];
+  // Parsing arguments
+  while (argc > 0) {
     if ((*argv)[0] == '-') {
-      switch ((*argv)[1]) {
-      case 'h': // -h = help flag
-        flag_help = 1;
-        break;
-      case 'v': // -v = version flag
-        flag_version = 1;
-        break;
-      case 'd': // -d = debug build flag
-        flag_debug = 1;
-        break;
-      case 'r': // -r = release build flag
-        flag_release = 1;
-        break;
-      default: // -??? = any unknown flag is displayed as unknown
-        fprintf(stderr, "[ERR] Unknown flag: -%c\n", (*argv)[1]);
-        return EXIT_FAILURE;
+      // Parsing flags
+      if (!strcmp(*argv, "-h") || !strcmp(*argv, "--help"))
+        f_help = 1;
+      else if (!strcmp(*argv, "-v") || !strcmp(*argv, "--version"))
+        f_version = 1;
+      else if (!strcmp(*argv, "-m") || !strcmp(*argv, "--make"))
+        f_make = 1;
+      else if (!strcmp(*argv, "-d") || !strcmp(*argv, "--debug"))
+        f_debug = 1;
+      else if (!strcmp(*argv, "-r") || !strcmp(*argv, "--release"))
+        f_release = 1;
+      else {
+        fprintf(stderr, "[ERR] Unknown flag: %s\n", *argv);
+        f_help = 1;
+        f_err = 1;
       }
-    } else { // If it is not flag we write it into a table of projects to make
-      if (project - projects >= MAX_PROJECTS) {
-        fprintf(stderr, "[ERR] Too many projects (max %d),\n", MAX_PROJECTS);
-        return EXIT_FAILURE;
-      }
-      size_t argv_length = strlen(*argv) + 1;
-      *project = (char *)malloc(argv_length * sizeof(char));
-      if (*project == NULL) {
-        fprintf(stderr, "[ERR] The 'malloc' function failed for: %s\n", *argv);
-        for (char **cleanup = projects; cleanup < project; cleanup++) {
-          free(*cleanup);
-        }
-        return EXIT_FAILURE;
-      }
-      strncpy(*project, *argv, argv_length);
-      project++;
+    } else {
     }
+    // Going through each argument
     argv++;
+    argc--;
   }
-  /// Main CLI response processor
-  if (flag_help) {
-    fprintf(stdout, USAGE_STRING);
-    return EXIT_SUCCESS;
+  if (f_help)
+    fprintf(stdout, STRING_USAGE);
+  if (f_version) {
+    fprintf(stdout, "[INF] CMaykr version : ");
+    fprintf(stdout, "%d.%d.%d\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
   }
-  if (flag_version) {
-    fprintf(stdout, "[INF] CMaykr version: " VERSION_STRING);
-    return EXIT_SUCCESS;
+  if (f_debug && f_release) {
+    fprintf(stderr, "[ERR] Cannot build both release and debug version.\n");
+    f_err = 1;
   }
-  if (flag_debug && flag_release) {
-    fprintf(stderr, "[ERR] Cannot use both -d and -r flags.\n");
+  if (f_make) {
+  }
+  if (f_debug) {
+  }
+  if (f_release) {
+  }
+  if (f_err)
     return EXIT_FAILURE;
-  }
-  if (flag_debug)
-    fprintf(stdout, "[INF] Producing debug build. -> TODO!\n");
-  if (flag_release)
-    fprintf(stdout, "[INF] Producing release build. -> TODO!\n");
-  /// Making the projects (parser to config.cmaykr file)
-  for (char **config_pointer = projects; config_pointer < project;
-       config_pointer++) {
-    char config_path[CONFIG_PATH_LENGTH];
-    snprintf(config_path, sizeof(config_path), "%s/config.maykr",
-             *config_pointer);
-    FILE *config_file = fopen(config_path, "r");
-    if (!config_file) {
-      fprintf(stderr, "[ERR] Could not open config file: %s\n", config_path);
-      continue; // We will skip a project that does not have a configuration file
-    }
-    fprintf(stdout, "[INF] Parsing config: %s\n", config_path);
-
-    fclose(config_file);
-  }
-  /// Correct cleanup
-  for (char **cleanup = projects; cleanup < project; cleanup++) {
-    free(*cleanup);
-  }
   return EXIT_SUCCESS;
 }
